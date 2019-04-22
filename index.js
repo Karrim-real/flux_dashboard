c23fb7d1ee7a9e50c338422dc1b4cfe182567de1
Akinyemi <karrim4real@gmail.com>
2019-04-22 16:06:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+	$('btn-success').click(function(){
+		
+	})
+})
+
+
+// function getdata(){
+// 	// body...
+
+// 	$.ajax({
+
+// 		url: "https://api.ravepay.co/v2/services/confluence",
+// 		dataType: 'json',
+// 		secret_key: "FLWSECK-dcf3ce0327fb688599a7634627a58564-X",
+// 		  service: "rates_convert",
+// 		  service_method: "post",
+// 		  service_version: "v1",
+// 		  service_channel: "transactions",
+// 		  service_channel_group: "merchants",
+// 		  service_payload: {
+// 		    FromCurrency: "USD",
+// 		    ToCurrency: "NGN",
+// 		    Amount: 5000
+// 		  }
+		
+// 	})
+// }
